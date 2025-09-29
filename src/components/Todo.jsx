1540795192eecc32ac318f7c03bdf97c9929c5ec
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-09-29 08:19:26 +0700

    penambahan

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo()  {
+  const [todos, setTodos] = useState([
+    {id: 1, text: 'Learning Todo App V2', time: '08:00 AM', date: '', completed: false}
+  ])
+  const [newTodo, setNewTodo] = useState('')
+
+  // Function for add todo
+  const addTodo = () => {
+    if(newTodo.trim() == '')
+    return;
+    const currentDate = new Date();
+    const timeString = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    const dateString = currentDate.toLocaleDateString();
+
+    setTodos ([
+      {
+        text: newTodo,
+        id: Date.now(),
+        time: `${timeString}, ${dateString}`,
+        completed: false
+      },
+      ...todos
+    ])
+    setNewTodo('')
+  }
+
+  // Delete Todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  // Toggle Complete
+  const completeTodo = (id) => {
+    setTodos(todos.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo));
+  }
+
   return (
-    <div>
-      
+    <div className="">
+
     </div>
   )
 }
